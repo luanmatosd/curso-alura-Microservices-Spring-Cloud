72ab47ead12602be7c61d99451e407c33c1ae26e
Luan Matos <luanmatos.ti@gmail.com>
2024-03-13 10:20:44 -0300

    Adding Interface Logger

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +16,27 @@ import br.com.alura.microservice.loja.model.Compra;
 
 @Service
 public class CompraService {
-
+	
+	//Dessa forma, estamos colocando o LOG em cada instância de serviço
+	//Ou seja, precisarei acessar instância por instância para poder enxergar os Logs
+	//O ideal aqui seria agrupar essa Logs e colocar em um único arquivo
+	//Iremos fazer agora, com que cada instância envie seu Log para um servidor central de Logs
+	//Para isso iremos usar o Papertrail
+	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+	
 	@Autowired
 	private FornecedorClient fornecedorClient;
 
 	public Compra realizaCompra(CompraDTO compra) {
 		
-		// Buscando informações do endereço do fornecedor para realizar uma compra
-		InfoFornecedorDTO infoFornecedorDTO = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		final String estado = compra.getEndereco().getEstado();
 		
+		LOG.info("Buscando informações de fornecedor do {}", estado);
+		// Buscando informações do endereço do fornecedor para realizar uma compra
+		InfoFornecedorDTO infoFornecedorDTO = fornecedorClient.getInfoPorEstado(estado);
+		
+		
+		LOG.info("Realizando um pedido");
 		// Fazendo pedido ao fornecedor passando os itens da compra
 		// e buscando "id" e "tempo de preparo" do pedido
 		InfoPedidoDTO infoPedidoDTO = fornecedorClient.realizaPedido(compra.getItens());
